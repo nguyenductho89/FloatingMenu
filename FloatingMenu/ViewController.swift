185f1f6f635818f314cbc9373f770efc4dd2f213
nguyenductho89 <nguyenductho89@gmail.com>
2021-10-30 02:27:59 +0700

    re

--- FloatingMenu/ViewController.swift
+++ FloatingMenu/ViewController.swift
@@ -50,16 +50,32 @@ final class FloatingMenu {
         menuWindow.makeKeyAndVisible()
     }
     
-    private let buttonSize: CGSize = CGSize(width: 100.0, height: 30.0)
-    private let buttonOrigin: CGPoint = CGPoint(x: UIScreen.main.bounds.size.width - 120,
-                                                y: UIScreen.main.bounds.size.height - 50)
+    private static let buttonSize: CGSize = CGSize(width: 100.0, height: 30.0)
+    private let buttonOrigin: CGPoint = CGPoint(x: UIScreen.main.bounds.size.width
+                                                - FloatingMenu.buttonSize.width
+                                                - FloatingMenu.trailingMargin,
+                                                y: UIScreen.main.bounds.size.height
+                                                - FloatingMenu.buttonSize.height
+                                                - FloatingMenu.bottomMargin)
+    private static let leadingMargin = 20.0
+    private static let trailingMargin = 20.0
+    private static let topMargin = 30.0
+    private static let bottomMargin = 20.0
+    private var scaleX: CGFloat {
+        let expandMenuWidth = UIScreen.main.bounds.width - FloatingMenu.leadingMargin - FloatingMenu.trailingMargin
+        return expandMenuWidth/FloatingMenu.buttonSize.width
+    }
+    private var scaleY: CGFloat {
+        let expandMenuHeight = UIScreen.main.bounds.height - FloatingMenu.topMargin - FloatingMenu.bottomMargin
+        return expandMenuHeight/FloatingMenu.buttonSize.height
+    }
     lazy var menuWindow: UIWindow = {
         guard let keyWindowScene = UIApplication.shared.keyWindowScene else {
             fatalError("Recheck keywindowscene")
         }
         let window = UIWindow(windowScene: keyWindowScene)
         window.frame = CGRect(origin: buttonOrigin,
-                              size: buttonSize)
+                              size: FloatingMenu.buttonSize)
         let menuVC = MenuViewController()
         menuVC.setExpand = {[weak self] isExpand in
             guard let self = self else {return}
@@ -76,10 +92,11 @@ final class FloatingMenu {
     private func expandAnimation() {
         UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn) {[weak self] in
             guard let self = self else {return}
-            let offset = UIOffset(horizontal: -self.buttonSize.width/2.0,
-                                  vertical: -self.buttonSize.height/2.0)
-            let scaleTransform = CGAffineTransform(scaleX: 2, y: 2)
-            let translateTransform = CGAffineTransform(translationX: offset.horizontal, y: offset.vertical)
+            let offset = UIOffset(horizontal: -FloatingMenu.buttonSize.width,
+                                  vertical: -FloatingMenu.buttonSize.height)
+            let scaleTransform = CGAffineTransform(scaleX: self.scaleX, y: self.scaleY)
+            let translateTransform = CGAffineTransform(translationX: offset.horizontal,
+                                                       y: offset.vertical)
             self.menuWindow.transform = scaleTransform.concatenating(translateTransform)
         }
     }
